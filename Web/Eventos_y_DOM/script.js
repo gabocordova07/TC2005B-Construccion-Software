c2871986477c591f5f5e6232c68a9091f8b691ec
gabocordova07 <70386592+gabocordova07@users.noreply.github.com>
2022-03-13 22:04:13 -0600

    Update eventos DOM

--- Web/Eventos_y_DOM/script.js
+++ Web/Eventos_y_DOM/script.js
@@ -1,15 +1,39 @@
-function findScreenCoords(mouseEvent)
+function getFormvalue()
 {
-    window.addEventListener('mousemove', function (e)
-    {
-        document.getElementById("mousePosition").textContent = e.x + ", " + e.y;
-    }); 
+    let fname = document.getElementById("form1").elements[0].value;
+    let lname = document.getElementById("form1").elements[1].value;
+
+    fullName = fname + " " + lname;
+
+    const p = document.createElement("p");
+    const text = document.createTextNode(fullName);
+    p.appendChild(text);
+
+    document.getElementById("form1").appendChild(p);
 }
 
+function insertRow()
+{
+    let table = document.getElementById("sampleTable");
+    let newRow = table.insertRow(0);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    cell1.innerHTML = " ";
+    cell2.innerHTML = " ";
+}
+
+function insertColumn()
+{
+    
+}
 
 function main()
 {
-    findScreenCoords();
+    window.addEventListener('mousemove', function (e)
+    {
+        document.getElementById("mousePosition").textContent = "Posición del mouse: " + e.x + ", " + e.y;
+    }); 
+
 }
 
 main();
